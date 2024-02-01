1dc2b9bc62732187eae4af5a92287fa8d9f30a6e
Mikhail Davydov <mdavydov83@gmail.com>
2024-02-01 15:28:47 +0100

    Valid Anagram 1st working solution but not fastest

---
+++ Valid Anagram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neetcode.io
+{
+    internal class ValidAnagram
+    {
+
+        
+        public bool IsAnagram(string s, string t)
+        {
+
+            if (s.Length != t.Length) { return false; }
+
+            int stringLength = s.Length;
+            for (int i = 0; i < stringLength; i++)
+            {
+                char c = s[i];
+                int indexOfChar = t.IndexOf(c);
+                try { t = t.Remove(indexOfChar, 1); }
+                catch { return false; }
+            }
+            return true;
+        }
+    }
+}
