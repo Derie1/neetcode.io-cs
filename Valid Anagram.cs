4f924c76dc3b4c551a98996a11a59c545560108f
Mikhail Davydov <mdavydov83@gmail.com>
2024-02-02 12:59:59 +0100

    TwoSums solved

--- Valid Anagram.cs
+++ Valid Anagram.cs
@@ -8,12 +8,26 @@ namespace neetcode.io
 {
     internal class ValidAnagram
     {
+        /*Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
-        
+        An Anagram is a word or phrase formed by rearranging the letters of a different word or 
+        phrase, typically using all the original letters exactly once. 
+
+        Example 1:
+
+        Input: s = "anagram", t = "nagaram"
+        Output: true
+        Example 2:
+
+        Input: s = "rat", t = "car"
+        Output: false
+        */
+
         public bool IsAnagram(string s, string t)
         {
 
             if (s.Length != t.Length) { return false; }
+            if (s == t) { return true; }
 
             int stringLength = s.Length;
             for (int i = 0; i < stringLength; i++)
